31436622a0c2139da9da3746cb242f21b866c19c
André Mamprin Mori <andremori16@gmail.com>
2020-11-15 20:41:34 +0000

    Updated logic to add one Yellow tile in the start

--- src/logic.pl
+++ src/logic.pl
@@ -1,22 +1,40 @@
 /*Initial functions*/
 initialize :-
+    % Initialize board
     initialState(GameState),
+
+    % Start setting yellow pieces on the board
     displayGame(GameState,'Player 1'),
+
+    setYellowPiece(GameState, NewGameState),
+    displayGame(NewGameState,'Player 2').
+
+
+
+setYellowPiece(GameState, NewGameState) :-
     getGameBoard(GameState, GameBoard),
-    selectTile(GameBoard, Value).
+
+    % Player chooses tile to put yellow piece
+    selectTile(GameBoard, Tile, Row, Col),
+    % TODO check if tile is not on the edge
+    replaceInMatrix(GameBoard, Row, Col, 'yellow', NewGameBoard),
+
+    % Update GameState
+    setGameBoard(GameState, NewGameBoard, NewGameState).
 
 
-selectPiece(GameBoard,Value) :-
-    getValueBoard(GameBoard,NewValue),
-    validateValue(NewValue,Value,GameBoard).
+/* AUX FUNCTIONS */
+selectPiece(GameBoard, Value, Row, Col) :-
+    getValueBoard(GameBoard, NewValue, Row, Col),
+    validateValue(NewValue, Value, GameBoard).
 
 
-selectTile(GameBoard, Tile) :-
-    getValueBoard(GameBoard,NewTiles),
-    validateTile(NewTiles,Value,GameBoard).
+selectTile(GameBoard, Tile, Row, Col) :-
+    getValueBoard(GameBoard, NewTiles, Row, Col),
+    validateTile(NewTiles, Value, GameBoard).
 
 
-getValueBoard(GameBoard,Value) :-
+getValueBoard(GameBoard,Value, Row, Col) :-
     manageColumn(Col),
     manageRow(Row),
     getValueFromMatrix(GameBoard,Row,Col,Value).
